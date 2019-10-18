1b8a226e188e1e5043def6a3cde0cf593b40f322
Fabio Tavares <tavarescomtudo@gmail.com>
2019-10-18 01:44:15 +0100

    Repeat activity created

---
+++ app/src/main/java/com/fjln1920/smarclock/Activities/Repeat.kt
@@ -0,0 +1,13 @@
+package com.fjln1920.smarclock.Activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.fjln1920.smarclock.R
+
+class Repeat : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_repeat)
+    }
+}

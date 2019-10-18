2e00ecac6d3f7e35c1b4740c3f043e146b850e0f
Nuno Tavares Andrade <nunatass@gmail.com>
2019-10-18 01:14:47 +0100

    my breanch was created

---
+++ app/src/main/java/com/fjln1920/smarclock/Activities/ChooseAGame.kt
@@ -0,0 +1,12 @@
+package com.fjln1920
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ChooseAGame : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_choose_agame)
+    }
+}

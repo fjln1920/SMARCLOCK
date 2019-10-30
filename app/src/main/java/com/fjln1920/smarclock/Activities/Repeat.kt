7cbeea7f8a342b2c7b56357ee441027b97687535
Fabio Tavares <tavarescomtudo@gmail.com>
2019-10-30 13:37:17 +0000

    o que acham como está ?

--- app/src/main/java/com/fjln1920/smarclock/Activities/Repeat.kt
+++ app/src/main/java/com/fjln1920/smarclock/Activities/Repeat.kt
@@ -4,50 +4,63 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
 import  android.os.Handler
-import android.widget.Toast
-import androidx.constraintlayout.solver.widgets.Helper
-import androidx.core.os.postDelayed
+import android.os.Looper
+import android.util.Log
 import com.fjln1920.smarclock.R
-import com.fjln1920.smarclock.Utils.Utils
 import java.util.*
+import kotlin.concurrent.schedule
 
 class Repeat : AppCompatActivity() {
 
     private lateinit var shapeImg: ImageView
-    private var shapeSequence = intArrayOf(0,1,2,3);
+    private val sequence1: ByteArray = byteArrayOf(0,1, 2, 3)
+    private val sequence2: ByteArray = byteArrayOf(2, 3, 1, 0)
+    private val sequence3: ByteArray = byteArrayOf(0, 3, 2, 1)
+    private val sequence4: ByteArray = byteArrayOf(0, 1, 3, 2)
+
+    private val shapeArray: IntArray = intArrayOf(
+        R.drawable.ic_circle,
+        R.drawable.ic_square,
+        R.drawable.ic_triangle,
+        R.drawable.ic_x
+    )
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_repeat)
-
-
-
-
         shapeImg = findViewById(R.id.repeat_shape_image_big)
+        //shapeImg.setImageResource(R.drawable.ic_circle)
+        var arrayToUse = getRandonSequenceArray()
+        showToMemorize(3000, arrayToUse)
 
 
-        var i=0
-        while (i<4){
 
-
-
-                if (i == 0)
-                    shapeImg.setImageResource(R.drawable.ic_circle)
-                if (i == 1)
-                    shapeImg.setImageResource(R.drawable.ic_triangle)
-                if (i == 2)
-                    shapeImg.setImageResource(R.drawable.ic_square)
-                if (i == 3)
-                    shapeImg.setImageResource(R.drawable.ic_x)
-        
-            i-=-1
-        }
 
     }
 
 
+    fun changeByShapeIndex(shape: Int) {
+        shapeImg.setImageResource(shapeArray[shape])
+    }
 
+
+    private fun showToMemorize(time: Long, arrayToUse: ByteArray) {
+
+
+
+    }
+
+
+    private fun getRandonSequenceArray(): ByteArray {
+        return when ((0..3).random()) {
+            0 -> sequence1
+            1 -> sequence2
+            2 -> sequence3
+            else -> sequence4
+        }
+
+    }
 
 
 }
